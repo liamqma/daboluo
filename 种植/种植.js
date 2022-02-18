c6271860916460735428c16af089dd63c2702f41
Liam Ma <liamma@192-168-1-125.tpgi.com.au>
2022-02-18 14:54:32 +1100

    更新种植的图片

--- 种植/种植.js
+++ 种植/种植.js
@@ -1,6 +1,6 @@
-if(!requestScreenCapture()){
-    toast("请求截图失败");
-    exit();
+if (!requestScreenCapture()) {
+  toast("请求截图失败");
+  exit();
 }
 
 var imgShouGe = images.read("收割.jpeg");
@@ -9,62 +9,95 @@ var imgShiFei = images.read("施肥.jpeg");
 var imgZaCao = images.read("杂草.jpeg");
 var imgKuHuang = images.read("枯黄.jpeg");
 
-var utils = require('../utils');
+var utils = require("../utils");
 var choosePeople = utils.choosePeople;
 var clickOnButtonUnderTheDialog = utils.clickOnButtonUnderTheDialog;
 var xuanWuPin = utils.xuanWuPin;
 
+var zhuangKuangX = 845;
+var zhuangKuangY = 265;
+var zhuangKuangWidth = 400;
+var zhuangKuangHeight = 60;
+
 var number = parseInt(rawInput("请输入种植次数", "5"));
 
 if (number) {
-    toast("循环" + number + "次")
-    for(var count = 0; count < number; count ++) {
-        toast("草地");
-        choosePeople(1);
-        toast("种植");
+  toast("循环" + number + "次");
+  for (var count = 0; count < number; count++) {
+    toast("草地");
+    choosePeople(1);
+    toast("种植");
+    clickOnButtonUnderTheDialog(2);
+    xuanWuPin(1);
+    sleep(5000);
+    toast("种的东西");
+    choosePeople(2);
+
+    while (true) {
+      var screenshot = captureScreen();
+      var pShouGe = findImage(screenshot, imgShouGe, {
+        region: [1017, 462, 300, 85],
+        threshold: 0.7,
+      });
+      var pChongYao = findImage(screenshot, imgChongYao, {
+        region: [
+          zhuangKuangX,
+          zhuangKuangY,
+          zhuangKuangWidth,
+          zhuangKuangHeight,
+        ],
+        threshold: 0.9,
+      });
+      var pShiFei = findImage(screenshot, imgShiFei, {
+        region: [
+          zhuangKuangX,
+          zhuangKuangY,
+          zhuangKuangWidth,
+          zhuangKuangHeight,
+        ],
+        threshold: 0.9,
+      });
+      var pZaCao = findImage(screenshot, imgZaCao, {
+        region: [
+          zhuangKuangX,
+          zhuangKuangY,
+          zhuangKuangWidth,
+          zhuangKuangHeight,
+        ],
+        threshold: 0.9,
+      });
+      var pKuHuang = findImage(screenshot, imgKuHuang, {
+        region: [
+          zhuangKuangX,
+          zhuangKuangY,
+          zhuangKuangWidth,
+          zhuangKuangHeight,
+        ],
+        threshold: 0.9,
+      });
+
+      if (pShouGe) {
+        clickOnButtonUnderTheDialog(1);
+        sleep(10000);
+        break;
+      } else if (pChongYao) {
+        clickOnButtonUnderTheDialog(4);
+        sleep(10000);
+      } else if (pShiFei) {
+        clickOnButtonUnderTheDialog(3);
+        sleep(10000);
+      } else if (pZaCao) {
+        clickOnButtonUnderTheDialog(1);
+        sleep(10000);
+      } else if (pKuHuang) {
         clickOnButtonUnderTheDialog(2);
-        xuanWuPin(1);
+        sleep(10000);
+      } else {
+        toast("正常");
         sleep(5000);
-        toast("种的东西");
-        choosePeople(2);
-
-        while (true) {
-            var screenshot = captureScreen();
-            var pShouGe = findImage(screenshot, imgShouGe, { region: [ 1017, 462, 300, 85 ], threshold: 0.7 });
-            var pChongYao = findImage(screenshot, imgChongYao, { region: [ 840, 270, 400, 60 ], threshold: 0.7 });
-            var pShiFei = findImage(screenshot, imgShiFei, { region: [ 840, 270, 400, 60 ], threshold: 0.7 });
-            var pZaCao = findImage(screenshot, imgZaCao, { region: [ 840, 270, 400, 60 ], threshold: 0.7 });
-            var pKuHuang = findImage(screenshot, imgKuHuang, { region: [ 840, 270, 400, 60 ], threshold: 0.7 });
-
-            if (pShouGe) {
-                toast("收割");
-                clickOnButtonUnderTheDialog(1);
-                sleep(10000)
-                break;
-            } else if (pChongYao) {
-                toast("虫咬");
-                clickOnButtonUnderTheDialog(4)
-                sleep(10000)
-            } else if (pShiFei) {
-                toast("施肥");
-                clickOnButtonUnderTheDialog(3)
-                sleep(10000)
-            } else if (pZaCao) {
-                toast("杂草");
-                clickOnButtonUnderTheDialog(1)
-                sleep(10000)
-            } else if (pKuHuang) {
-                toast("叶子枯黄");
-                clickOnButtonUnderTheDialog(2)
-                sleep(10000)
-            } else {
-                toast("正常");
-                sleep(5000)
-            }
-        }
+      }
     }
+  }
 } else {
-    toast("invalid input")
+  toast("invalid input");
 }
-
-

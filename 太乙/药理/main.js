2f7e4aea6b095210fd06173beefbea25d07c017f
Liam Ma <liamma@192-168-1-125.tpgi.com.au>
2022-02-19 21:06:37 +1100

    药理添加答案

--- 太乙/药理/main.js
+++ 太乙/药理/main.js
@@ -21,8 +21,12 @@ var answers = [
   "茯苓",
   "金银花白色",
   "鳖甲",
+  "杜仲",
+  "木瓜",
+  "何首乌",
+  "燕窝",
+  "牛膝",
 ];
-var screenshot = captureScreen();
 
 var qStartingX = 318;
 var qStartingY = 186;
@@ -41,50 +45,65 @@ var a2StartY = 551;
 var a3StartY = 662;
 var a4StartY = 771;
 
+var screenshot = captureScreen();
+
 var pQ;
 var pA;
-for (var i = 0; i < answers.length; i++) {
-  var answer = answers[i];
-  var imgQ = images.read(answer + "q.jpg");
-  pQ = findImage(screenshot, imgQ, {
-    region: [qStartingX, qStartingY, qWidth, qHeight],
-    threshold: 0.7,
-  });
 
-  if (pQ) {
-    var imgA = images.read(answer + "a.jpg");
-    pA = findImage(screenshot, imgA, {
-      region: [aStartX, aStartY, aWidth, aHeight],
+for (var count = 1; count <= 10; count++) {
+  pQ = undefined;
+  pA = undefined;
+  for (var i = 0; i < answers.length; i++) {
+    var answer = answers[i];
+    var imgQ = images.read(answer + "q.jpg");
+    pQ = findImage(screenshot, imgQ, {
+      region: [qStartingX, qStartingY, qWidth, qHeight],
       threshold: 0.7,
     });
-    if (pA) {
-      toast("找到答案了");
-      sleep(2000);
-      click(pA.x + 100, pA.y + 20);
-      break;
+
+    if (pQ) {
+      var imgA = images.read(answer + "a.jpg");
+      pA = findImage(screenshot, imgA, {
+        region: [aStartX, aStartY, aWidth, aHeight],
+        threshold: 0.7,
+      });
+      if (pA) {
+        toast("找到答案了");
+        sleep(2000);
+        click(pA.x + 100, pA.y + 20);
+        sleep(5000);
+        break;
+      }
     }
   }
-}
 
-if (!pA) {
-  var answer = dialogs.select("请选择一个选项", [1, 2, 3, 4]);
-  var qClip = images.clip(screenshot, qStartingX, qStartingY, qWidth, qHeight);
-  var aClip;
-  switch (answer) {
-    case 0:
-      aClip = images.clip(screenshot, a1StartX, a1StartY, a1Width, a1Height);
-      break;
-    case 1:
-      aClip = images.clip(screenshot, a1StartX, a2StartY, a1Width, a1Height);
-      break;
-    case 2:
-      aClip = images.clip(screenshot, a1StartX, a3StartY, a1Width, a1Height);
-      break;
-    case 3:
-      aClip = images.clip(screenshot, a1StartX, a4StartY, a1Width, a1Height);
-      break;
+  if (!pA) {
+    var answer = dialogs.select("请选择一个选项", [1, 2, 3, 4]);
+    var qClip = images.clip(
+      screenshot,
+      qStartingX,
+      qStartingY,
+      qWidth,
+      qHeight
+    );
+    var aClip;
+    switch (answer) {
+      case 0:
+        aClip = images.clip(screenshot, a1StartX, a1StartY, a1Width, a1Height);
+        break;
+      case 1:
+        aClip = images.clip(screenshot, a1StartX, a2StartY, a1Width, a1Height);
+        break;
+      case 2:
+        aClip = images.clip(screenshot, a1StartX, a3StartY, a1Width, a1Height);
+        break;
+      case 3:
+        aClip = images.clip(screenshot, a1StartX, a4StartY, a1Width, a1Height);
+        break;
+    }
+    images.save(qClip, "../../../../q.jpg", "jpg", 100);
+    images.save(aClip, "../../../../a.jpg", "jpg", 100);
+    toast("保存好图片了");
+    break;
   }
-  images.save(qClip, "../../../../q.jpg", "jpg", 100);
-  images.save(aClip, "../../../../a.jpg", "jpg", 100);
-  toast("保存好图片了");
 }
